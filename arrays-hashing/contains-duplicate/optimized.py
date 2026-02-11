8630ffe97543eb13b2b7d60790c466d5b083a440
ph10rabelo <ph10rabelo@gmail.com>
2026-02-11 07:00:52 -0300

    Create optimized.py
    Essa eh a solucao otimizada gerada por ia, que utiliza conjuntos inves de um for duplo em uma lista. Sendo assim, a complexidade de tempo eh bem otimizada, caindo para O(n), que ocorre no for n in nums, com finalidade de acessar todos os elementos da lista. Apos isso, para ver se o elemento ja foi visto ou nao, se utiliza uma tabela hash e nao um for, o que reduz consideravelmente o tempo. Mas nada na computacao vem de graca, essa solucao possui um gasto de memoria consideravelmente maior, por conta da utilizacao da tabela hash pelo set. Por conta de sua indexacao nao ser  linear, acaba gastando mais que um de espaco na memoria por elemento.

---
+++ arrays-hashing/contains-duplicate/optimized.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        vistos = set() 
+        for n in nums:
+            if n in vistos
+                return True
+            vistos.add(n)
+        return False
