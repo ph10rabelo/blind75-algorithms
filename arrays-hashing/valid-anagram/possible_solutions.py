bc18650bc320804506d473e3f9a405012060b438
ph10rabelo <ph10rabelo@gmail.com>
2026-02-15 01:58:00 -0300

    Create possible_solutions.py
    A primeira solução seria bem acessível se eu tivesse utilizado a função sorted(). Embora já conhecesse essa função, não me ocorreu utilizá-la para normalizar as strings, permitindo uma comparação direta entre as listas ordenadas.
    
    Já a segunda abordagem é uma solução muito elegante utilizando dicionários . Ela utiliza os caracteres da palavra como chaves e a sua frequência como valor. Dessa forma, é possível mapear exatamente quais letras aparecem e quantas vezes cada uma se repete. Como eu ainda não tinha total domínio sobre o funcionamento dos dicionários, não consegui idealizar essa lógica , mas pretendo me aprofundar nessa estrutura, pois ela otimiza drasticamente a busca e a contagem de dados.

---
+++ arrays-hashing/valid-anagram/possible_solutions.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        return sorted(s) == sorted(t) #solucao mais simples, mas com complexidade de tempo O(m log m+ n log n)
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT #solucao mais dificil de pensar, mas com complexidade de tempo O(m+n)
